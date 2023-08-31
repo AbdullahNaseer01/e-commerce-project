89623b832c8b93bf57bd5578834e1a2a2cedc2f0
abdullahnaseer01 <mianabdullah125125@gmail.com>
2023-09-01 01:32:39 +0500

    refactoring and making dynamic routes

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import { AuthUserProvider } from '../../firebase/Auth';
+import Navbar from './(components)/Navbar';
+import Footer from './(components)/Footer';
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -12,9 +14,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+      <Navbar/>
        {/* <AuthUserProvider> */}
         {children}
         {/* </AuthUserProvider> */}
+        <Footer/>
         </body>
     </html>
   )
