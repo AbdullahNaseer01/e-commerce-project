7f58adcb836f39962c649de4f461734c9a296d60
abdullahnaseer01 <mianabdullah125125@gmail.com>
2023-09-02 17:29:11 +0500

    refactoring

--- firebase/firebaseConfig.js
+++ firebase/firebaseConfig.js
@@ -37,5 +37,5 @@ const firebaseConfig = {
 
 
 export const app = initializeApp(firebaseConfig);
-export const database = getFirestore(app)
+export const db = getFirestore(app)
 export const auth = getAuth(app)
