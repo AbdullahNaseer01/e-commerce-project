b850a4db8becdb2e449590a6a44b1c6bf49568c5
abdullahnaseer01 <mianabdullah125125@gmail.com>
2023-09-02 17:52:14 +0500

    read data added

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,5 @@
+'use client'
+import { useEffect } from "react";
 import BannerSection from "./(components)/BannerSection";
 import BlogSection from "./(components)/BlogSection";
 import Category from "./(components)/Category";
@@ -7,7 +9,28 @@ import FeatureSectionFruits from "./(components)/FeatureSectionFruits";
 import Hero from "./(components)/Hero";
 import Newsletter from "./(components)/NewsLetter";
 
+import { collection, query, where, getDocs } from "firebase/firestore";
+import {db} from "../../firebase/firebaseConfig.js"
+
 export default function Home() {
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const q = query(collection(db, "products"), where("category", "==", "fruits"));
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        console.log(doc.id, " => ", doc.data());
+        console.log("useeffect run")
+      });
+    };
+  
+    fetchData(); // Call the async function immediately
+  
+  }, []);
+  
+
+
   return (
     <main>
       <Hero />
