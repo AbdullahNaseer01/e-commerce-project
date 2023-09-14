09bdb309661c66766e7b358d32e99e0d3b14e8ac
abdullahnaseer01 <mianabdullah125125@gmail.com>
2023-09-14 08:32:49 +0500

    data fetch accrding to category

--- src/app/(client)/[category]/page.js
+++ src/app/(client)/[category]/page.js
@@ -5,67 +5,6 @@ import { collection, where, query, getDocs } from "firebase/firestore";
 import { db } from "../../../../firebase/firebaseConfig";
 
 
-// // const page = ({params}) => {
-// //   return (
-// //     <div>page {params.category} </div>
-// //   )
-// // }
-
-// // export default page
-
-// const Page = ({params}) => {
-//   const router = useRouter();
-//   // const { category } = router.query; // Get the category parameter from the URL
-//   const category = params.category || 'defaultCategory'; // Use a default value if 'category' is undefined
-
-//   const [loading, setLoading] = useState(false);
-//   const [products, setProducts] = useState([]);
-
-//   const fetchData = async (category) => {
-//     setLoading(true);
-//     const q = query(collection(db, "products"), where("category", "==", category));
-//     const querySnapshot = await getDocs(q);
-//     const productsData = [];
-//     querySnapshot.forEach((doc) => {
-//       productsData.push({ id: doc.id, ...doc.data() });
-//     });
-//     setProducts(productsData);
-//     setLoading(false);
-//     console.log(productsData);
-//     console.log(products);
-//   };
-  
-
-//   useEffect(() => {
-//     if (category) {
-//       fetchData(category); // Fetch data when the category changes in the URL
-//       console.log(category);
-
-
-//     }
-//   }, [category]);
-
-
-
-
-//   return (
-//     <div>
-//       <h1>Products in Category: {category}</h1>
-//       {loading ? (
-//         <div>Loading...</div>
-//       ) : (
-//         <ul>
-//           {products.map((product) => (
-//             <li key={product.id}>{product.name}</li>
-//           ))}
-//         </ul>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default Page;
-
 const Page = ({params}) => {
   const router = useRouter();
   const category = params.category || 'defaultCategory';
@@ -112,7 +51,9 @@ const Page = ({params}) => {
     });
   
     // Update state using the callback form
-    setProducts((prevProducts) => [...prevProducts, ...productsData]);
+    // setProducts((prevProducts) => [...prevProducts, ...productsData]);
+    // or
+    setProducts(productsData);
   
     setLoading(false);
     console.log(productsData);
@@ -139,11 +80,59 @@ console.log("Category after fetchData:", category);
       {loading ? (
         <div>Loading...</div>
       ) : (
-        <ul>
-          {products.map((product) => (
-            <li key={product.id}>{product.title}</li>
-          ))}
-        </ul>
+        // <ul>
+        //   {products.map((product) => (
+        //     <li key={product.id}>{product.title}</li>
+        //   ))}
+        // </ul>
+        <section
+        // id="Projects"
+        className="w-fit mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5"
+      >
+        {products.map((product) =>(
+          <div key={product.id} className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+          <a href="#">
+            <img
+              src={product.imageFile}
+             alt="Product"
+              className="h-80 w-72 object-cover rounded-t-xl"
+            />
+            <div className="px-4 py-3 w-72">
+              <span className="text-gray-400 mr-3 uppercase text-xs">Brand</span>
+              <p className="text-lg font-bold text-black truncate block capitalize">
+                {product.title}
+              </p>
+              <div className="flex items-center">
+                <p className="text-lg font-semibold text-black cursor-auto my-3">
+                  $149
+                </p>
+                <del>
+                  <p className="text-sm text-gray-600 cursor-auto ml-2">$199</p>
+                </del>
+                <div className="ml-auto">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width={20}
+                    height={20}
+                    fill="currentColor"
+                    className="bi bi-bag-plus"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z"
+                    />
+                    <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
+                  </svg>
+                </div>
+              </div>
+            </div>
+          </a>
+        </div>
+        ))
+
+        }     
+      </section>
       )}
     </div>
   );
--- src/app/(client)/components/Category.jsx
+++ src/app/(client)/components/Category.jsx
@@ -4,51 +4,59 @@ import Link from "next/link";
 const data = [
   {
     id: 0,
-    name: "fruits",
+    name: "Fresh Fruits",
     count: "9 Products",
     img: "../images/category__1.webp",
+    category:"fruits"
   },
   {
     id: 1,
     name: "Fresh Vegs",
     count: "8 Products",
     img: "../images/category__2.webp",
+    category:"vegetables"
   },
   {
     id: 2,
     name: "Canned Goods",
     count: "10 Products",
     img: "../images/category__3.webp",
+    category:"canned-food"
   },
   {
     id: 3,
     name: "Bread & Bakery",
     count: "12 Products",
     img: "../images/category__4.webp",
+    category:"bakery-items",
   },
   {
     id: 4,
     name: "Fishes",
     count: "10 Products",
     img: "../images/category__5.webp",
+    category:"fishes",
   },
   {
     id: 5,
     name: "Eggs & Dairy",
     count: "7 Products",
     img: "../images/category__6.webp",
+    category:"egg-and-dairy",
   },
   {
     id: 6,
     name: "Soft Drinks",
     count: "5 Products",
     img: "../images/category__7.webp",
+    category:"soft-drinks-snacks",
   },
   {
     id: 7,
     name: "Fresh Fruits",
     count: "9 Products",
     img: "../images/category__1.webp",
+    category:"others",
   },
 ];
 
@@ -57,7 +65,7 @@ const Category = () => {
     <div className="container pt-16">
       <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {data.map((el) => (
-        <Link href={`/${el.name}`}  >
+        <Link href={`/${el.category}`}  >
           <CategoryCard
             key={el.id}
             img={el.img}
