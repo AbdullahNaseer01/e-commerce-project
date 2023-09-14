8d1bcb3276e1ece4c986b130b1567272ddb37b1a
abdullahnaseer01 <mianabdullah125125@gmail.com>
2023-09-14 08:35:47 +0500

    deployment error debugging

--- src/app/(client)/components/Category.jsx
+++ src/app/(client)/components/Category.jsx
@@ -63,9 +63,9 @@ const data = [
 const Category = () => {
   return (
     <div className="container pt-16">
-      <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      <div  className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {data.map((el) => (
-        <Link href={`/${el.category}`}  >
+        <Link key={el.id} href={`/${el.category}`}  >
           <CategoryCard
             key={el.id}
             img={el.img}
