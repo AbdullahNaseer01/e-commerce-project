0e3760506db4c3fbbe94429fd9404a00319dd48b
abdullahnaseer01 <mianabdullah125125@gmail.com>
2023-09-28 00:52:12 +0500

    findin bug

--- src/app/(client)/components/RegisterModel.jsx
+++ src/app/(client)/components/RegisterModel.jsx
@@ -77,8 +77,92 @@ const RegisterModel = () => {
   return isLoading || (!isLoading && authUser) ? (
     "Loading"
   ) : (
-    <div className="flex items-center justify-center h-screen">
-      <div className="bg-customYellow p-8 rounded shadow-md max-w-sm w-full">
+    // <div className="flex items-center justify-center h-screen fixed inset-0  z-50">
+    //   <div className="bg-customYellow p-8 rounded shadow-md max-w-sm w-full">
+    //     <h1 className="text-2xl font-semibold mb-4">Sign Up</h1>
+
+    //     <form>
+    //       <div className="mb-4">
+    //         <label
+    //           htmlFor="username"
+    //           className="block text-sm font-medium text-gray-700"
+    //         >
+    //           Username
+    //         </label>
+    //         <input
+    //           type="text"
+    //           id="username"
+    //           name="username"
+    //           required
+    //           value={username}
+    //           onChange={handleUsernameChange}
+    //           className="mt-1 py-2 block w-full rounded-md drop-shadow-2xl focus:outline-none"
+    //         />
+    //       </div>
+    //       <div className="mb-4">
+    //         <label
+    //           htmlFor="email"
+    //           className="block text-sm font-medium text-gray-700"
+    //         >
+    //           Email
+    //         </label>
+    //         <input
+    //           type="email"
+    //           id="email"
+    //           name="email"
+    //           required
+    //           value={email}
+    //           onChange={handleEmailChange}
+    //           className="mt-1 py-2 block w-full rounded-md drop-shadow-2xl focus:outline-none"
+    //         />
+    //       </div>
+    //       <div className="mb-4">
+    //         <label
+    //           htmlFor="password"
+    //           className="block text-sm font-medium text-gray-700"
+    //         >
+    //           Password
+    //         </label>
+    //         <input
+    //           type="password"
+    //           id="password"
+    //           name="password"
+    //           required
+    //           value={password}
+    //           onChange={handlePasswordChange}
+    //           className="mt-1 py-2 block w-full rounded-md drop-shadow-2xl focus:outline-none"
+    //         />
+    //       </div>
+    //       <button
+    //         type="submit"
+    //         className="w-full py-2 px-4 bg-customDarkYellow hover:bg-moreDarkYellow focus:ring-opacity-50 drop-shadow-2xl"
+    //         onClick={handleSignup}
+    //       >
+    //         Sign Up
+    //       </button>
+    //     </form>
+    //     <button
+    //       type="submit"
+    //       className="w-full mt-3 py-2 px-4 bg-white hover:bg-gray-200 focus:ring-opacity-50 drop-shadow-2xl flex justify-evenly"
+    //       onClick={signInWithGoogle}
+    //     >
+    //       <span className="mt-1">
+    //         <FcGoogle />
+    //       </span>{" "}
+    //       continue with Google
+    //     </button>
+    //     <button
+    //       onClick={() => {
+    //         router.push("/login");
+    //       }}
+    //       className="w-full mt-3 py-2 px-4 text-slate-400 hover:text-slate-500"
+    //     >
+    //       Already have an account?
+    //     </button>
+    //   </div>
+    // </div>
+    // <div className="flex items-center justify-center h-screen fixed inset-0  z-50">
+      <div className="bg-customYellow p-8 rounded shadow-md max-w-sm w-full fixed   z-50">
         <h1 className="text-2xl font-semibold mb-4">Sign Up</h1>
 
         <form>
@@ -160,7 +244,7 @@ const RegisterModel = () => {
           Already have an account?
         </button>
       </div>
-    </div>
+
   );
 };
 
